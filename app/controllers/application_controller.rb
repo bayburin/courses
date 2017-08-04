29f3be62fb2d8b1037a988857e8fb0f13b656bfd
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-08-04 16:01:00 +0700

    Add gem 'devise'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  layout :layout
+
+  # Определяем, какой layout выводить: для входа в систему или основной
+  def layout
+    is_a?(Devise::SessionsController) || is_a?(Devise::RegistrationsController) || is_a?(Devise::ConfirmationsController) ? 'sign_in_app' : 'application'
+  end
 end
